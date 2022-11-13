22151fb2a775c269af00a7bb5f73289d858b34d5
Mahima Ramireddy <106462040+MahimaRamireddy@users.noreply.github.com>
2022-11-13 14:27:44 +0530

    BMI-calculator.py

--- BMI-Calculator.py
+++ BMI-Calculator.py
@@ -8,22 +8,29 @@ def weight():
     weight=float(E2.get())
     return weight
 
-def calculate_BMI(a=""):
-    print(a)
-    try:
-        Height=height()
-        Weight=weight()
-        bmi=Weight/(Height*Height)
-    except ZeroDivisionError:
-        messagebox.showinfo("Error", "Please enter positive height!!")
-    except ValueError:
-        messagebox.showinfo("Error", "Please enter valid data!")
+def calculate_BMI():
+    
+
+    Height=height()
+    Weight=weight()
+    bmi=Weight/(Height*Height)
+    
+    BMI=Label(top)
+    BMI.config(text=bmi)
+    BMI.place(x=125,y=200)
+         
+def check_valid():
+    if(E1.get() and E2.get()):
+        if(height()>0 and weight()>0):
+           calculate_BMI()
+        elif(height()<=0 or weight<=0):
+           messagebox.showinfo("Error", "Please enter valid data!")
     else:
-        BMI=Label(top)
-        BMI.config(text=bmi)
-        BMI.place(x=200,y=242)
-         
-         
+        messagebox.showinfo("Error", "Please fill the empty fields!")
+    
+    
+    
+             
 
 top=Tk()
 #top.configure(background="beige")
@@ -44,8 +51,9 @@ E2.place(x=125,y=100)
 bmi=Label(top,text="BMI")
 bmi.place(x=0,y=200)
 
-BUTTON = Button(text=" Calculate",command=calculate_BMI)
+BUTTON = Button(text=" Calculate",command=check_valid)
 BUTTON.grid(row=3, column=0, sticky=W)
 BUTTON.place(x=100,y=300)
 
 top.mainloop()
+
